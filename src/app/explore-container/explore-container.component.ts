c8cce795fe1788a836b931132f1fac5de70482dd
sipelepis <moran.phillipjan@ue.edu.ph>
2024-03-06 19:04:03 +0800

    Initial Commit

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -8,5 +8,8 @@ import { Component, Input } from '@angular/core';
 export class ExploreContainerComponent {
 
   @Input() name?: string;
+  public onUploadChange(){
 
+    console.log('onUploadChange');
+  }
 }
