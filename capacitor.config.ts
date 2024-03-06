5d3dfc2f3b489783bc1d8f7576e4a03e39132f25
sipelepis <moran.phillipjan@ue.edu.ph>
2024-03-06 18:39:22 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reproducer_fileInput',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
