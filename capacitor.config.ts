a0498d88bfa649cc58b831f71ab0c01470b46820
sipelepis <moran.phillipjan@ue.edu.ph>
2024-03-07 15:16:12 +0800

    add new packages

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,13 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'reproducer_fileInput',
   webDir: 'www',
+  bundledWebRuntime: false,
+  ios: {
+    limitsNavigationsToAppBoundDomains: true,
+  },
   server: {
     androidScheme: 'https'
-  }
+  },
 };
 
 export default config;
